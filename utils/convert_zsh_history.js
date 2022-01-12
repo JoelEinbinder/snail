f1fbf3235c1b3f557f1daa79000c12b73744ed83
Joel Einbinder <joel.einbinder@gmail.com>
2022-01-12 04:52:28 -0500

    convert history util

---
+++ utils/convert_zsh_history.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('fs');
+if (!process.argv[2]) {
+  console.error('missing .zsh_history path')
+  process.exit(1);
+}
+console.time();
+const content = fs.readFileSync(process.argv[2], 'utf8');
+
+const lines = [];
+let start = 0;
+for (let i = 0; i < content.length; i++) {
+  if (content[i] === '\\') {
+    i++;
+    continue;
+  }
+  if (content[i] === '\n') {
+    lines.push(content.slice(start, i));
+    start = i + 1;
+  }
+}
+lines.push(content.slice(start));
+const out = [];
+for (const line of lines) {
+  if (!line)
+    continue;
+  const match = line.match(/^: (\d+):(\d+);(.*)$/m);
+  const [, start, elapsed, cmd] = match;
+  out.push({start: parseInt(start) * 1000, cmd});
+}
+console.log(out.map(l => JSON.stringify(l)).join('\n'));
