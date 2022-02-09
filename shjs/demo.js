b65c0f746bd9e854bc5c6ca4468ca48c8aa446b0
Joel Einbinder <joel.einbinder@gmail.com>
2022-02-09 00:14:26 -0500

    cd builtin

--- shjs/demo.js
+++ shjs/demo.js
@@ -1,2 +1,5 @@
 const {execute} = require('./index');
 const {stdin, closePromise, kill} = execute(process.argv[2]);
+closePromise.then(c => {
+  process.exitCode = c;
+});
--- shjs/runner.js
+++ shjs/runner.js
@@ -1,5 +1,18 @@
 const {spawn} = require('child_process');
 const {Writable, Readable} = require('stream')
+
+/** @type {Object<string, (args: string[], stdout: Writable, stderr: Writable) => Promise<number>>} */
+const builtins = {
+    cd: async (args, stdout, stderr) => {
+        try {
+            process.chdir(args[0]);
+        } catch (e) {
+            stderr.write(e.message + '\n');
+            return 1;
+        }
+        return 0;
+    },
+}
 /**
  * @param {import('./ast').Expression} expression
  * @param {Writable} stdout
@@ -9,17 +22,32 @@ const {Writable, Readable} = require('stream')
  */
 function execute(expression, stdout, stderr, stdin) {
     if ('executable' in expression) {
-        const child = spawn(expression.executable, expression.args, {
-            stdio: [stdin === process.stdin ? 'inherit' : 'pipe', stdout === process.stdout ? 'inherit' : 'pipe', stderr === process.stderr ? 'inherit' : 'pipe'],
-        });
-        const closePromise = new Promise(resolve => child.on('close', resolve));
-        if (stdin !== process.stdin && stdin)
-            stdin.pipe(child.stdin, { end: false });
-        if (stderr !== process.stderr)
-            child.stderr.pipe(stderr, { end: false });
-        if (stdout !== process.stdout)
-            child.stdout.pipe(stdout, { end: false });
-        return {stdin: child.stdin, kill: child.kill.bind(child), closePromise};
+        if (expression.executable in builtins) {
+            const closePromise = builtins[expression.executable](expression.args, stdout, stderr);
+            return {
+                closePromise,
+                stdin: new Writable({write(){}}),
+                kill: () => void 0,
+            }
+        } else {
+            const child = spawn(expression.executable, expression.args, {
+                stdio: [stdin === process.stdin ? 'inherit' : 'pipe', stdout === process.stdout ? 'inherit' : 'pipe', stderr === process.stderr ? 'inherit' : 'pipe'],
+            });
+            const closePromise = new Promise(resolve => {
+                child.on('close', resolve);
+                child.on('error', err => {
+                    stderr.write(err.message + '\n');
+                    resolve(127);
+                });
+            });
+            if (stdin !== process.stdin && stdin)
+                stdin.pipe(child.stdin, { end: false });
+            if (stderr !== process.stderr)
+                child.stderr.pipe(stderr, { end: false });
+            if (stdout !== process.stdout)
+                child.stdout.pipe(stdout, { end: false });
+            return {stdin: child.stdin, kill: child.kill.bind(child), closePromise};
+        }
     } else if ('pipe' in expression) {
         const pipe = execute(expression.pipe, stdout, stderr);
         const main = execute(expression.main, pipe.stdin, stderr, stdin);
