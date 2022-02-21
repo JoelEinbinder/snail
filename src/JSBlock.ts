c769e4778b98725829eae4ab4493685dcf0bfc1b
Joel Einbinder <joel.einbinder@gmail.com>
2022-02-20 22:14:05 -0500

    full remote object rendering

--- src/JSBlock.ts
+++ src/JSBlock.ts
@@ -33,23 +33,87 @@ function renderRemoteObject(object: Protocol.Runtime.RemoteObject, connection: J
       if (object.subtype === 'null')
         return renderBasicRemoteObject(object);
       return renderObjectRemoteObject(object, connection, willResize);
-    // case 'function':
+    case 'function':
+      return renderOther('Function');
     default:
       console.log('unknown', object);
       return document.createElement('div');
   }
 }
 
-function renderObjectRemoteObject(object: Protocol.Runtime.RemoteObject, connection: JSConnection, willResize: JoelEvent<void>) {
+function renderObjectRemoteObject(object: Protocol.Runtime.RemoteObject, connection: JSConnection, willResize: JoelEvent<void>, prefix?: Element) {
   const details = document.createElement('details');
-  const summary = renderRemoteObjectSummary(object);
+  const summary = renderRemoteObjectSummary(object, prefix);
   details.appendChild(summary);
+  const content = document.createElement('div');
+  content.classList.add('content');
+  let populated = false;
+  async function populate() {
+    if (populated)
+      return;
+    populated = true;
+    const {result, exceptionDetails, internalProperties, privateProperties} = await connection.send('Runtime.getProperties', {
+      objectId: object.objectId,
+      generatePreview: true,
+      ownProperties: true,
+    });
+    if (details.open)
+      willResize.dispatch();
+    for (const property of result)
+      addProperty(property);
+    for (const property of privateProperties || [])
+      addProperty({configurable: false, enumerable: false, ...property}, true);
+    for (const property of internalProperties || [])
+      addProperty({configurable: false, enumerable: false, ...property}, true);
+    function addProperty(property: Protocol.Runtime.PropertyDescriptor, dim = false) {
+      const propertyLabel = document.createElement('span');
+      const propertyName = document.createElement('span');
+      propertyName.classList.add('property-name');
+      if (dim)
+        propertyName.classList.add('dim');
+      if (property.symbol)
+        propertyName.classList.add('symbol');
+      propertyName.append(property.name);
+      propertyLabel.append(propertyName, ': ');
+      if (property.value && property.value.type === 'object' && property.value.subtype !== 'null') {
+        content.appendChild(renderObjectRemoteObject(property.value, connection, willResize, propertyLabel));
+      } else {
+        const div = document.createElement('div');
+        div.classList.add('property');
+        div.appendChild(propertyLabel);
+        if (property.value) {
+          div.appendChild(renderRemoteObject(property.value, connection, willResize));
+        } else {
+          const types = [];
+          if (property.get)
+            types.push('Getter');
+          if (property.set)
+            types.push('Setter');
+          div.appendChild(renderOther(types.join('/')));
+        }
+        content.appendChild(div);
+      }
+    }
+  }
+  details.addEventListener('toggle', () => {
+    willResize.dispatch();
+    if (details.open) {
+      populate();
+      details.appendChild(content);
+    } else {
+      details.removeChild(content);
+    }      
+  }, false);
   return details;
 }
 
-function renderRemoteObjectSummary(object: Protocol.Runtime.RemoteObject) {
+function renderRemoteObjectSummary(object: Protocol.Runtime.RemoteObject, prefix?: Element) {
   const summary = document.createElement('summary');
+  if (prefix)
+    summary.appendChild(prefix);
   if (object.preview) {
+    if (object.preview.subtype !== 'array' && object.className !== 'Object')
+      summary.append(object.className, ' ');
     summary.append(object.preview.subtype === 'array' ? '[ ' : '{ ');
     let first = true;
     let overflow = object.preview.overflow
@@ -74,11 +138,10 @@ function renderRemoteObjectSummary(object: Protocol.Runtime.RemoteObject) {
           summary.append(renderOther('Function'));
           break;
         case 'object':
-          if (property.subtype === 'null') {
+          if (property.subtype === 'null')
             summary.append(renderBasicRemoteObject(property));
-            break;
-          }
-          summary.append(renderOther(property.value));
+          else
+            summary.append(renderOther(property.value));
           break;
         case 'accessor':
           summary.append(renderOther('Getter'));
@@ -90,7 +153,7 @@ function renderRemoteObjectSummary(object: Protocol.Runtime.RemoteObject) {
     }
     summary.append(object.preview.subtype === 'array' ? ' ]' : ' }');
   } else {
-    summary.textContent = object.description;
+    summary.append(renderOther(object.description));
   }
   return summary;
 }
