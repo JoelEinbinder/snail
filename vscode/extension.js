eddb8be3cc3e7972f50b8ab123d207f76b8092b8
Joel Einbinder <joel.einbinder@gmail.com>
2022-03-29 02:37:13 -0400

    add extension info

--- vscode/extension.js
+++ vscode/extension.js
@@ -10,20 +10,6 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "joel-terminal" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	subscribe(vscode.commands.registerCommand('joel-terminal.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from joel-terminal!');
-	}));
-
 	subscribe(vscode.window.registerWebviewViewProvider('joel-terminal.terminal', {
 		resolveWebviewView: (webview, cancellation) => {
 			webview.webview.cspSource;
