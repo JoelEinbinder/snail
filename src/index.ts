83424b35ee8a59af5d8523ff258352178bf1a9a0
Joel Einbinder <joel.einbinder@gmail.com>
2022-09-03 03:01:58 -0400

    instrument module load time

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import { rootBlock } from './GridPane';
 
 const isLogBook = document.location.search.includes('logbook');
 if (!isLogBook) {
-  const shellPromise = import('./Shell').then(({ Shell }) => Shell.create());
+  console.time('load shell module');
+  const shellPromise = import('./Shell').then(({ Shell }) => {
+    console.timeEnd('load shell module');
+    return Shell.create();
+  });
   import('./LogView').then(async ({LogView}) => {
     const shell = await shellPromise;
     const logView = new LogView(shell, rootBlock.element);
