b97a38e4170b76e542c2bf907aa4a74302b24711
Joel Einbinder <joel.einbinder@gmail.com>
2022-11-16 12:09:11 -0500

    TargetManager for debugger

---
+++ debugger/TargetManager.ts
@@ -0,0 +1,144 @@
+/// <reference path="../iframe/types.d.ts" />
+import {RPC} from '../src/RPC';
+import { WebKitProtocol } from '../src/webkitProtocol';
+
+interface TargetListener {
+  targetAdded(target: WebKitTarget): void;
+  targetRemoved(target: WebKitTarget): void;
+}
+
+export class TargetManager {
+  private _targets = new Map<string|undefined, WebKitTarget>();
+  private _listeners = new Set<TargetListener>();
+  constructor() {
+    this.startup();
+  }
+  
+  addListener(listener: TargetListener) {
+    this._listeners.add(listener);
+    for (const target of this._targets.values())
+      listener.targetAdded(target);
+  }
+
+  removeListener(listener: TargetListener) {
+    this._listeners.delete(listener);
+  }
+
+  async startup() {
+    const sendMessage = await d4.attachToCDP({
+      onDebuggeesChanged: debuggees => {
+        console.log('onDebuggeesChanged', debuggees);
+        const newTargets = new Map<string|undefined, WebKitTarget>();
+        const framesForBrowserView = new Map<string|undefined, Set<string|undefined>>();
+        const addTarget = (browserViewUUID?: string) => {
+          const existingTarget = this._targets.get(browserViewUUID);
+          if (existingTarget)
+            newTargets.set(browserViewUUID, existingTarget);
+          else
+            newTargets.set(browserViewUUID, new WebKitTarget(sendMessage));
+          framesForBrowserView.set(browserViewUUID, new Set());
+        }
+        // always add undefined target for the main page
+        addTarget(undefined);
+        for (const [shellUUID, debuggee] of Object.entries(debuggees)) {
+          if (!newTargets.has(debuggee.browserViewUUID))
+            addTarget(debuggee.browserViewUUID);
+          framesForBrowserView.get(debuggee.browserViewUUID)!.add(debuggee.frameUUID);
+        }
+        const oldTargets = this._targets;
+        this._targets = newTargets;
+        for (const [key, value] of oldTargets) {
+          if (newTargets.has(key)) continue;
+          for (const listener of this._listeners)
+            listener.targetRemoved(value);
+        }
+        for (const [key, value] of newTargets) {
+          if (oldTargets.has(key)) continue;
+          for (const listener of this._listeners)
+            listener.targetAdded(value);
+        }
+        for (const [browserViewUUID, frames] of framesForBrowserView)
+          this._targets.get(browserViewUUID)!._updateFrames(frames);
+      },
+      onMessage: (message, browserViewUUID) => {
+        console.log('onMessage', message);
+        this._targets.get(browserViewUUID)!._listener(message);
+      }
+    });
+  }
+}
+
+export type WebKitSession = RPC<{
+  [key in keyof WebKitProtocol.CommandParameters]: (params: WebKitProtocol.CommandParameters[key]) => WebKitProtocol.CommandReturnValues[key];
+}, WebKitProtocol.Events>;
+export interface WebKitTargetListener {
+  sessionUpdated(session: WebKitSession): void;
+  frameAdded(frameUUID: string|undefined): void;
+  frameRemoved(frameUUID: string|undefined): void;
+}
+
+export class WebKitTarget {
+  frames = new Set<string|undefined>();
+  private topLevelRPC: WebKitSession;
+  rpc?: WebKitSession;
+  _listener: (message: any) => void;
+  private _listeners = new Set<WebKitTargetListener>();
+  constructor(send: (message: any) => void) {
+    this.topLevelRPC = new RPC({
+      listen: listener => {
+        this._listener = listener;
+      },
+      send,
+    });
+    const rpcListenerForPage = new Map<string, (message: any) => void>();
+    this.topLevelRPC.on('Target.targetCreated', event => {
+      if (event.targetInfo.type !== 'page')
+        return;
+      if (event.targetInfo.isProvisional)
+        return;
+      this.rpc = new RPC({
+        listen: (listener) => {
+          rpcListenerForPage.set(event.targetInfo.targetId, listener);
+        },
+        send: (message) => {
+          this.topLevelRPC.send('Target.sendMessageToTarget', {
+            message: JSON.stringify(message),
+            targetId: event.targetInfo.targetId,
+          });
+        }
+      });
+      for (const listener of this._listeners)
+        listener.sessionUpdated(this.rpc);
+    })
+  }
+
+  addListener(listener: WebKitTargetListener) {
+    this._listeners.add(listener);
+    if (this.rpc)
+      listener.sessionUpdated(this.rpc);
+    for (const frame of this.frames)
+      listener.frameAdded(frame);
+  }
+
+  removeListener(listener: WebKitTargetListener) {
+    this._listeners.delete(listener);
+  }
+
+  _updateFrames(frames: Set<string|undefined>) {
+    const oldFrames = this.frames;
+    this.frames = frames;
+    for (const frame of oldFrames) {
+      if (this.frames.has(frame))
+        continue;
+      for (const listener of this._listeners)
+        listener.frameRemoved(frame);
+    }
+    for (const frame of this.frames) {
+      if (oldFrames.has(frame))
+        continue;
+      for (const listener of this._listeners)
+        listener.frameAdded(frame);
+    }
+  }
+}
+
--- debugger/web.ts
+++ debugger/web.ts
@@ -1,76 +1,24 @@
 /// <reference path="../iframe/types.d.ts" />
-import {RPC} from '../src/RPC';
+import {TargetManager} from './TargetManager';
 document.body.textContent = 'im the web debugger';
-class WebKitSender {
-  public rawSend: (message: any) => void;
-  private _callbacks = new Map<number, (value: any) => void>();
-  private _lastId = 0;
-  async send(method, params = undefined) {
-    const id = ++this._lastId;
-    this.rawSend({
-      method,
-      params,
-      id,
-    });
-    const data = await new Promise<{result: any}|{error: any}>(callback => {
-      this._callbacks.set(id, callback);
-    });
-    if ('error' in data)
-      throw new Error(`WebKit Protocol Error: ${data.error}`);
-    return data.result;
-  }
-  onMessage(message) {
-    console.log(message);
-    const {method, params, id, result, error} = message;
-    if (method) {
-
-    }
-  }
-}
-let rawSend;
-type TargetInfo = {
-  targetId: string;
-  type: 'page'|'service-worker'|'worker';
-  isProvisional?: boolean;
-  isPaused?: boolean;
-};
-
-const rpc = new RPC<{
-  'Target.sendMessageToTarget': (params: {message: string, targetId: string}) => void;
-}, {
-  'Target.targetCreated': { targetInfo: TargetInfo },
-  'Target.targetDestroyed': { targetId: string },
-  'Target.dispatchMessageFromTarget': { targetId: string, message: string },
-  'Target.didCommitProvisionalTarget': { newTargetId: string, oldTargetId: string },
-}>({
-  async listen(listener) {
-    rawSend = await d4.attachToCDP({
-      onDebuggeesChanged(debuggees) {
-        console.log('onDebuggeesChanged', debuggees);
+const targetManager = new TargetManager();
+targetManager.addListener({
+  targetAdded(target) {
+    console.log('targetAdded', target);
+    target.addListener({
+      sessionUpdated(session) {
+        console.log('session', session);
       },
-      onMessage(message, browserViewUUID) {
-        console.log('onMessage', message);
-        listener(message);
+      frameAdded(frameUUID) {
+        console.log('frameAdded', frameUUID);
+      },
+      frameRemoved(frameUUID) {
+        console.log('frameRemoved', frameUUID);
       }
     });
   },
-  send(message) {
-    rawSend(message);
-  }
-});
-rpc.on('Target.targetCreated', async ({targetInfo}) => {
-  console.log('target created');
-  rpc.send('Target.sendMessageToTarget', {
-    targetId: targetInfo.targetId,
-    message: JSON.stringify({
-      method: 'DOM.getDocument',
-      params: {},
-      id: 2,
-    })
-  });
-});
-rpc.on('Target.dispatchMessageFromTarget', async({message, targetId}) => {
-  console.log(JSON.parse(message));
-});
-
+  targetRemoved(target) {
+    console.log('targetRemoved', target);
+  },
+})
 export {};
