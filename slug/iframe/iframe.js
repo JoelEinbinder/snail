f9b28861786b8d1205ea7ac8a05a42934b29559c
Joel Einbinder <joel.einbinder@gmail.com>
2023-10-18 20:59:41 -0400

    put foucs on iframe body by default

--- slug/iframe/iframe.js
+++ slug/iframe/iframe.js
@@ -332,6 +332,11 @@ function setFindHandler(_findHandler) {
   findHandler?.(findParams);
 }
 
+// Hack to put focus somewhere. Otherwise electron loses keyboard events?!
+document.body.tabIndex = -1;
+document.body.focus();
+document.body.removeAttribute('tabIndex');
+
 window.d4 = {
   waitForMessage,
   setHeight,
