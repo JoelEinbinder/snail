dcef4b5f5f74f93bab67e578facdf68f11da69bd
Joel Einbinder <joel.einbinder@gmail.com>
2023-10-21 00:05:50 -0400

    publish sdk script

---
+++ utils/publish-sdk.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const { execSync } = require('child_process');
+const path = require('path');
+
+(async () => {
+  execSync('npm publish', { cwd: path.join(__dirname, '..', 'slug', 'sdk') });
+})();
+
