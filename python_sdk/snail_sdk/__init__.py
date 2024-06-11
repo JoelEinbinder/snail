19e0a1fbc7f986414632f106daea7da024ce1bcb
Joel <joel.einbinder@gmail.com>
2024-06-11 03:22:01 -0400

    fix: flush python sdk messages

--- python_sdk/snail_sdk/__init__.py
+++ python_sdk/snail_sdk/__init__.py
@@ -1,17 +1,17 @@
 import json
 def display(filePath):
-    print('\x1b\x1aL' + str(filePath) + '\x00', end='')
+    print('\x1b\x1aL' + str(filePath) + '\x00', end='', flush=True)
 
 def set_progress(progress, left_text=None, right_text=None):
     print('\x1b\x1a\x4e' + json.dumps({
         'progress': progress,
         'leftText': left_text,
         'rightText': right_text
-    }) + '\x00', end='')
+    }) + '\x00', end='', flush=True)
 
 def chart(data):
-    print('\x1b\x1aC' + json.dumps(data) + '\x00', end='')
+    print('\x1b\x1aC' + json.dumps(data) + '\x00', end='', flush=True)
 
 def send(data, dont_cache=False):
     str = json.dumps(data)
-    print('\x1b\x1a' + ('Q' if dont_cache else 'M') + str + '\x00', end='')
+    print('\x1b\x1a' + ('Q' if dont_cache else 'M') + str + '\x00', end='', flush=True)
